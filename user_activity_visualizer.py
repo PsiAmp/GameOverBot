0b0b99e6f7a87e4be28dfb5cd2632b1737934685
PsiAmp <kostinkostya@gmail.com>
2022-03-27 20:04:44 +0300

    Refactored user activity scanner

--- user_activity_visualizer.py
+++ user_activity_visualizer.py
@@ -4,6 +4,107 @@ from datetime import datetime, timedelta
 import plotly.graph_objects as go
 
 
+class UserActivityScanner:
+
+    def __init__(self, username: str, date_from: datetime, date_to: datetime, scan_comments: bool, scan_submissions: bool):
+        self.username = username
+        self.date_from = date_from
+        self.date_to = date_to
+        self.scan_comments = scan_comments
+        self.scan_submissions = scan_submissions
+
+        # reddit login
+        self.reddit = praw_auth.authenticate()
+
+        # fetch submissions
+        self.user_data = self.scan_user()
+
+        # create data
+        self.show_graph()
+
+    def scan_user(self):
+        user_submissions = []
+        if self.scan_comments:
+            comments = self.fetch_comments()
+            user_submissions.extend(comments)
+        if self.scan_submissions:
+            submissions = self.fetch_submissions()
+            user_submissions.extend(submissions)
+        return user_submissions
+
+    def fetch_comments(self, limit=None):
+        comments = []
+        for comment in self.reddit.redditor(self.username).comments.new(limit=limit):
+            date = datetime.fromtimestamp(comment.created_utc)
+            if self.date_from and date < self.date_from:
+                break
+            if self.date_to and self.date_to < date:
+                continue
+
+            comments.append(comment)
+
+        return comments
+
+    def fetch_submissions(self, limit=None):
+        submissions = []
+        for submission in self.reddit.redditor(self.username).submissions.new(limit=limit):
+            date = datetime.fromtimestamp(submission.created_utc)
+            if self.date_from and date < self.date_from:
+                break
+            if self.date_to and self.date_to < date:
+                continue
+
+            submissions.append(submission)
+
+        return submissions
+
+    def days_old(self, t_utc: str):
+        today_date = datetime.today().date()
+        timestamp_date = datetime.fromtimestamp(t_utc)
+        days_diff = (today_date - timestamp_date.date()).days
+        return days_diff
+
+    def days_delta(self, d1: datetime, d2: datetime):
+        return abs((d1 - d2).days)
+
+    def create_z_data(self):
+        z = [[0] * 24 for _ in range(self.days_delta(self.date_to, self.date_from) + 1)]
+
+        for post in self.user_data:
+            hour = datetime.fromtimestamp(post.created_utc).hour
+            day_num = self.days_delta(self.date_to, datetime.fromtimestamp(post.created_utc))
+
+            try:
+                z[day_num][hour] += 1
+            except IndexError as e:
+                print(f'd:{day_num}; h:{hour}')
+
+            if z[day_num][hour] > 10:
+                z[day_num][hour] = 10
+
+        return z
+
+    def show_graph(self):
+        x = list(range(24))
+
+        y = []
+        for days in range(0, self.days_delta(self.date_to, self.date_from)):
+            d = self.date_to - timedelta(days=days)
+            y.append(d.strftime('%Y-%m-%d'))
+
+        z = self.create_z_data()
+
+        # show graph
+        fig = go.Figure(data=go.Heatmap(
+            z=z,
+            x=x,
+            y=y,
+            hoverongaps=False,
+            zmax=10
+        ))
+        fig.show()
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-debug', action="store_true", default=False)
@@ -12,92 +113,12 @@ def parse_args():
     is_debug = args.debug
 
 
-def fetch_comments(reddit, user_name, limit=None, date_start=None, date_end=None):
-    comments = []
-    for comment in reddit.redditor(user_name).comments.new(limit=limit):
-        date = datetime.fromtimestamp(comment.created_utc)
-        if date_start and date < date_start:
-            break
-        if date_end and date_end < date:
-            continue
-
-        comments.append(comment)
-
-    return comments
-
-
-def fetch_submissions(reddit, user_name, limit=None, max_days=None):
-    submissions = []
-    for submission in reddit.redditor(user_name).submissions.new(limit=limit):
-        if max_days is not None and max_days < days_old(submission.created_utc):
-            break
-
-        submissions.append(submission)
-
-    return submissions
-
-
-def days_old(t_utc):
-    today_date = datetime.today().date()
-    timestamp_date = datetime.fromtimestamp(t_utc)
-    days_diff = (today_date - timestamp_date.date()).days
-    return days_diff
-
-
-def days_delta(d1, d2):
-    return abs((d1 - d2).days)
-
-
-def create_z_data(posts, date_start, date_end):
-    z = [[0] * 24 for _ in range(days_delta(date_end, date_start) + 1)]
-
-    for post in posts:
-        hour = datetime.fromtimestamp(post.created_utc).hour
-        day_num = days_delta(date_end, datetime.fromtimestamp(post.created_utc))
-
-        try:
-            z[day_num][hour] += 1
-        except IndexError as e:
-            print(f'd:{day_num}; h:{hour}')
-
-        if z[day_num][hour] > 10:
-            z[day_num][hour] = 10
-
-    return z
-
-
 if __name__ == '__main__':
     # Parsing command line arguments
     parse_args()
 
-    # reddit login
-    reddit = praw_auth.authenticate()
-
-    date_end = datetime(2022, 3, 23)
-    date_start = datetime(2022, 2, 15)
-    username = 'username'
-
-    # fetch submissions
-    comments = fetch_comments(reddit, username, date_start=date_start, date_end=date_end)
-    #submissions = fetch_submissions(reddit, username, max_days=max_days)
-    #comments.extend(submissions)
-
-    # create data
-    x = list(range(24))
-
-    y = []
-    for days in range(0, days_delta(date_end, date_start)):
-        d = date_end - timedelta(days=days)
-        y.append(d.strftime('%Y-%m-%d'))
-
-    z = create_z_data(comments, date_start=date_start, date_end=date_end)
-
-    # show graph
-    fig = go.Figure(data=go.Heatmap(
-        z=z,
-        x=x,
-        y=y,
-        hoverongaps=False,
-        zmax=10
-    ))
-    fig.show()
+    u = UserActivityScanner(username='spez',
+                            date_from=datetime(2021, 1, 1),
+                            date_to=datetime.today(),
+                            scan_comments=True,
+                            scan_submissions=True)
